8a18302f8290905146d49d66c681e1b201846998
Matthew Edwards <matthew@matthewedwards.co.nz>
2017-01-11 13:39:21 +1300

    Tweaks
    * Fix browsersync, add date to permalink
    * Remove svgmin, use imagemin for svgs instead
    * Add Github link to author profile, set URL on production build
    * Rename yeoman.app -> dirs.app etc in Gruntfile
    * Set up jshint and csslint
    * Clean up Gruntfile, remove url from build config
    * Update jekyll version
    * Run verbose build on Travis

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source "http://rubygems.org"
 
-gem "jekyll", "3.3.0"
+gem "jekyll", "~>3.3.0"
 gem "mje-nz-mistakes-jekyll", "~>4.1.1.1"
--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
 // Generated on 2017-01-09 using generator-jekyllrb 1.4.1
+/*jslint node: true, esversion: 6 */
 'use strict';
 
 // Directory reference:
@@ -10,55 +11,29 @@
 module.exports = function (grunt) {
   // Show elapsed time after tasks run
   require('time-grunt')(grunt);
-  // Load all Grunt tasks
+
+  // Load all Grunt tasks (jit-grunt replaces load-grunt-tasks)
   require('jit-grunt')(grunt, {
     buildcontrol: 'grunt-build-control'
   });
 
   grunt.initConfig({
     // Configurable paths
-    yeoman: {
+    dirs: {
       app: 'app',
       dist: 'dist'
     },
-    watch: {
-      jekyll: {
-        files: [
-          '<%= yeoman.app %>/**/*',
-          '_config.yml'
-        ],
-        tasks: ['jekyll:server']
-      }
-    },
-    browserSync: {
-      server: {
-        options: {
-          server: {
-            baseDir: [
-              ".jekyll",
-            ]
-          },
-          watchTask: true
-        }
-      },
-      dist: {
-        options: {
-          server: {
-            baseDir: "<%= yeoman.dist %>"
-          }
-        }
-      }
-    },
+
     clean: {
       dist: {
         files: [{
           dot: true,
           src: [
-            '<%= yeoman.dist %>/*',
+            '<%= dirs.dist %>/*',
             // Running Jekyll also cleans the target directory.  Exclude any
             // non-standard `keep_files` here (e.g., the generated files
             // directory from Jekyll Picture Tag).
-            '!<%= yeoman.dist %>/.git*'
+            '!<%= dirs.dist %>/.git*'
           ]
         }]
       },
@@ -66,14 +41,15 @@ module.exports = function (grunt) {
         '.jekyll'
       ]
     },
+
     jekyll: {
       options: {
         config: '_config.yml,_config.build.yml',
-        src: '<%= yeoman.app %>'
+        src: '<%= dirs.app %>'
       },
       dist: {
         options: {
-          dest: '<%= yeoman.dist %>',
+          dest: '<%= dirs.dist %>',
         }
       },
       server: {
@@ -88,59 +64,93 @@ module.exports = function (grunt) {
         }
       }
     },
-    usemin: {
-      options: {
-        assetsDirs: ['<%= yeoman.dist %>', '<%= yeoman.dist %>/assets/img']
+
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: '.jekyll/**/*'  // Files to sync
+        },
+        options: {
+          server: '.jekyll',  // Folder to serve from
+          watchTask: true  // Allow other watch tasks to run after this task
+        }
       },
-      html: ['<%= yeoman.dist %>/**/*.html'],
-      css: ['<%= yeoman.dist %>/assets/css/**/*.css']
-    },
-    htmlmin: {
       dist: {
         options: {
-          collapseWhitespace: true,
-          collapseBooleanAttributes: true,
-          removeAttributeQuotes: true,
-          removeRedundantAttributes: true
-        },
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.dist %>',
-          src: '**/*.html',
-          dest: '<%= yeoman.dist %>'
-        }]
-      }
-    },
-    uncss: {
-      options: {
-        htmlroot: '<%= yeoman.dist %>'
-      },
-      dist: {
-        files: {
-          '<%= yeoman.dist %>/assets/css/main.css': ['<%= yeoman.dist %>/**/*.html']
+          server: '<%= dirs.dist %>'
         }
       }
     },
+
+    watch: {
+      jekyll: {
+        files: [
+          '<%= dirs.app %>/**/*',
+          '_config.yml'
+        ],
+        tasks: ['jekyll:server']
+      }
+    },
+
+    //TODO: This is supposed to run before the build, but the scripts are in
+    // the theme gem so not available until after build
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'Gruntfile.js',
+        '<%= dirs.app %>/assets/js/**/*.js'
+      ]
+    },
+
+    //TODO: This is supposed to run before the build, but the stylesheets are in
+    // the theme gem so not available until after build
+    csslint: {
+      options: {
+        csslintrc: '.csslintrc'
+      },
+      check: {
+        src: [
+          '<%= dirs.app %>/assets/css/**/*.css'
+        ]
+      }
+    },
+
+    uncss: {
+      options: {
+        htmlroot: '<%= dirs.dist %>'
+      },
+      dist: {
+        files: {
+          '<%= dirs.dist %>/assets/css/main.css': ['<%= dirs.dist %>/**/*.html']
+        }
+      }
+    },
+
     autoprefixer: {
       options: {
         browsers: ['last 2 versions', '> 1%']
       },
       dist: {
         files: {
-          '<%= yeoman.dist %>/assets/css/main.css': ['<%= yeoman.dist %>/assets/css/main.css'],
+          '<%= dirs.dist %>/assets/css/main.css': ['<%= dirs.dist %>/assets/css/main.css'],
         },
       }
     },
+
     cssmin: {
       dist: {
         files: {
-          '<%= yeoman.dist %>/assets/css/main.css': ['<%= yeoman.dist %>/assets/css/main.css'],
+          '<%= dirs.dist %>/assets/css/main.css': ['<%= dirs.dist %>/assets/css/main.css'],
         },
         options: {
           check: 'gzip'
         }
       }
     },
+
     imagemin: {
       dist: {
         options: {
@@ -148,22 +158,13 @@ module.exports = function (grunt) {
         },
         files: [{
           expand: true,
-          cwd: '<%= yeoman.dist %>',
-          src: '**/*.{jpg,jpeg,png}',
-          dest: '<%= yeoman.dist %>'
+          cwd: '<%= dirs.dist %>',
+          src: '**/*.{jpg,jpeg,png,gif,svg}',
+          dest: '<%= dirs.dist %>'
         }]
       }
     },
-    svgmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.dist %>',
-          src: '**/*.svg',
-          dest: '<%= yeoman.dist %>'
-        }]
-      }
-    },
+
     filerev: {
       options: {
         length: 4
@@ -171,14 +172,47 @@ module.exports = function (grunt) {
       dist: {
         files: [{
           src: [
-            '<%= yeoman.dist %>/assets/js/**/*.js',
-            '<%= yeoman.dist %>/assets/css/**/*.css',
-            '<%= yeoman.dist %>/assets/img/**/*.{gif,jpg,jpeg,png,svg,webp}',
-            '<%= yeoman.dist %>/assets/fonts/**/*.{eot*,otf,svg,ttf,woff}'
+            '<%= dirs.dist %>/assets/js/**/*.js',
+            '<%= dirs.dist %>/assets/css/**/*.css',
+            '<%= dirs.dist %>/assets/img/**/*.{gif,jpg,jpeg,png,svg,webp}',
+            '<%= dirs.dist %>/assets/fonts/**/*.{eot*,otf,svg,ttf,woff}'
           ]
         }]
       }
     },
+
+    usemin: {
+      options: {
+        assetsDirs: [
+          '<%= dirs.dist %>',  // Need this for absolute URLs
+          '<%= dirs.dist %>/assets/css'  // Need this for relative URLs
+        ]
+      },
+      html: ['<%= dirs.dist %>/**/*.html'],
+      css: ['<%= dirs.dist %>/assets/css/**/*.css']
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          removeAttributeQuotes: true,
+          removeRedundantAttributes: true,
+          removeComments: true,
+          processConditionalComments: true,
+          minifyJS: true,
+          processScripts: ['application/ld+json']
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= dirs.dist %>',
+          src: '**/*.html',
+          dest: '<%= dirs.dist %>'
+        }]
+      }
+    },
+
     buildcontrol: {
       dist: {
         options: {
@@ -188,26 +222,6 @@ module.exports = function (grunt) {
           push: true,
           connectCommits: false
         }
-      }
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
-      },
-      all: [
-        'Gruntfile.js',
-        '<%= yeoman.app %>/assets/js/**/*.js'
-      ]
-    },
-    csslint: {
-      options: {
-        csslintrc: '.csslintrc'
-      },
-      check: {
-        src: [
-          '<%= yeoman.app %>/assets/css/**/*.css'
-        ]
       }
     }
   });
@@ -219,18 +233,18 @@ module.exports = function (grunt) {
     }
 
     grunt.task.run([
-      'clean:server',
-      'jekyll:server',
-      'browserSync:server',
-      'watch'
+      'clean:server',  // Clean .jekyll
+      'jekyll:server',  // Build into .jekyll once
+      'browserSync:server',  // Start browsersync server
+      'watch'  // Watch app/, triggering jekyll:server on changes
     ]);
   });
 
   grunt.registerTask('check', [
-    'clean:server',
-    'jekyll:check',
-    'jshint:all',
-    'csslint:check'
+    'clean:server',  // Clean .jekyll
+    'jekyll:check',  // Run jekyll doctor
+    'jshint:all',  // Run jshint on (unbuilt) JS files
+    //'csslint:check'
     // 'scsslint'
   ]);
 
@@ -242,7 +256,6 @@ module.exports = function (grunt) {
     'autoprefixer',
     'cssmin',
     'imagemin',
-    'svgmin',
     'filerev',
     'usemin',
     'htmlmin'
@@ -257,24 +270,24 @@ module.exports = function (grunt) {
     }
 
     const exec_raw = require('child_process').execSync,
-          exec = function (cmd) {return exec_raw(cmd, {'encoding': 'utf8'}).trim()},
+          exec = function (cmd) {return exec_raw(cmd, {'encoding': 'utf8'}).trim();},
           current_branch = exec('git rev-parse --abbrev-ref HEAD'),
           current_repo = exec('git remote get-url origin'),
           buildcontrol_remote = grunt.config.get('buildcontrol.dist.options.remote'),
           unpushed_commits_list = exec('git cherry');
 
-    if (current_branch != 'master') {
+    if (current_branch !== 'master') {
      console.log(current_branch);
      grunt.log.error('Deploy error: Not on master branch (on ' + current_branch.trim() + ' instead)');
      return false;
     }
 
-    if (current_repo != buildcontrol_remote) {
+    if (current_repo !== buildcontrol_remote) {
       grunt.log.error('Deploy error: This repo\'s origin remote is ' + current_repo + ', buildcontrol is pushing to ' + buildcontrol_remote);
       return false;
     }
 
-    if (unpushed_commits_list != '') {
+    if (unpushed_commits_list !== '') {
       const unpushed_commits_count = unpushed_commits_list.split(/\r\n|\r|\n/).length;
       grunt.log.error('Deploy error: ' + unpushed_commits_count + ' unpushed commits');
       return false;
@@ -285,9 +298,8 @@ module.exports = function (grunt) {
   grunt.registerTask('deploy', [
     'checkenv',
     //'check',
-    //'test',
     'build',
-    'buildcontrol'
+    'buildcontrol'  // Push
     ]);
 
   grunt.registerTask('default', [
